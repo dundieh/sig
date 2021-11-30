e6dcb8ec52d8e2d03251a27061ec5d1ca5532282
ToGermanScore Ltd <yousef@togermanscore.com>
2021-11-30 12:36:33 +0200

    make it better

--- src/components/Ausbildung/Ausbildung.js
+++ src/components/Ausbildung/Ausbildung.js
@@ -20,6 +20,54 @@ class Ausbildung extends React.Component {
                             <div className="ui form ma0">
                                 <div className="fields">
                                     <h3>
+                                        الفترة اللى فاتت كذا حد سال علي معلومات عن Ausbildung او التدريب المهني في المانيا و الصراحه كمعلومات عنه قليله جدا ف كان لازم نبدا نخلي ناس اكتر تدور فى الموضوع دا يمكن نوصل لمعلومات كامله عنه زي اي موضوع قبل كدا  اتعرف
+                                        <br/>
+                                        البوست دا يمكن يبقي بدايه لطريق التدريب المهني من خلاله يمكن الناس تدور اكتر عنه وتوصل ان شاء الله لطريق جديد 
+                                        <br/>
+                                        هتكلم عن نقطتين بشكل عام معلومات عنه ودا هتلاقوة <a target='_blank' rel='noreferrer' href='https://www.dropbox.com/s/o2mnot6oqapwzq0/التدريب%20المهني%20في%20المانيا.rar?dl=0'>هنا</a> 
+                                        <br/>
+                                        و كمان معلومات كامله عن منحه شركه سيمنس المدفوعه  التقديم خلاص هينتهي كمان كام يوم و من باب العلم بالشئ المعلومات دي المرجعيه فيها ل
+                                        <br/>
+                                        <a target='_blank' rel='noreferrer' href='https://www.facebook.com/amr.monnos'>Amr El-Sherif</a>
+                                        <br/>
+                                        هيرد لو فى حاجه وقفت مع حد 
+                                        <br/>
+                                        وهوا عامل <a target='_blank' rel='noreferrer' href='https://youtu.be/JBMiRe2497o'>فيديو عنها بالتفاصيل</a>
+                                        <br/>
+                                        ______________________________________
+                                        <br/>
+                                        كمان دا رد لناس راسلتهم عشان تعرف داتا اكتر وبيقولوا محتاجين اثبات للمصاريف ف ركزوا ف النقط دى ( دا بعيدا عن منحه سيمنس )
+                                        <br/>
+                                        <div className='lft'>
+                                            Weil du aus einem so genannten Drittstaat kommst, hat dein Heimatland keine besonderen Abmachungen mit der EU und die Regeln sind etwas strenger. Aber keine Sorge, ich helfe dir auf deinem Weg dahin!
+                                            <br/>
+                                            So bewirbst du dich auf eine Ausbildung in Deutschland:
+                                            <br/>
+                                            1. Du bewirbst dich online für eine Ausbildung. Dafür musst du deine Zeugnisse auf Deutsch übersetzen lassen. Das geht über die Seite anabin 
+                                            <br/>
+                                            (<a target='_blank' rel='noreferrer' href='https://anabin.kmk.org/anabin-datenbank.html'>anabin.kmk.org/anabin-datenbank.html</a>).
+                                            <br/>
+                                            Auch eine Beglaubigung ist wichtig.
+                                            <br/>
+                                            So kann der Ausbilder in Deutschland deine Fähigkeiten besser einschätzen. Zusammen mit deinem Bewerbungsschreiben kannst du deine Zeugnisse dann online abschicken. Wenn du eine Stelle bekommst, musst du dich um ein Visum kümmern.
+                                            <br/>
+                                            2. Für das Visum kontaktierst du die deutsche Botschaft oder das Konsulat in deiner Nähe. Was du auf jeden Fall brauchst: genug Geld, um einige Zeit in Deutschland leben zu können (etwa 650 Euro pro Monat) und eine Krankenversicherung, die bis zum Ausbildungsstart gültig ist (in deiner Ausbildung suchst du dir dann hier eine deutsche Versicherung).
+                                            <br/>
+                                            Ich gebe dir einen Link zu unserem Blog, wo du alle Informationen noch einmal nachlesen kannst – auf Deutsch oder Englisch: <a target='_blank' rel='noreferrer' href='https://blog.ausbildung.de/ausbildung-in-deutschland/'>blog.ausbildung.de/ausbildung-in-deutschland</a>
+                                            <br/>
+                                            Auf unserer Seite findest du ganz viele offene Ausbildungsstellen – von A bis Z (<a target='_blank' rel='noreferrer' href='http://www.ausbildung.de/berufe/glossar/'>ausbildung.de/berufe/glossar</a>) oder nach Themen sortiert (<a target='_blank' rel='noreferrer' href='http://www.ausbildung.de/berufe/themen/'>ausbildung.de/berufe/themen</a>).
+                                            <br/>
+                                            Dort findest du dann für einen Beruf alle freien Plätze unter „freie Ausbildungsplätze“. Klickst du auf einen Betrieb, kannst du dich direkt bewerben. „Zur Stelle“ oder „Jetzt online bewerben“ steht dann auf der Seite. Dann draufklicken!
+                                            <br/>
+                                            Dein Deutsch ist schon echt super! Es gibt keine offizielle Regel, wie gut dein Deutsch sein muss. Es kommt immer auf den Chef und die Firma an, bei der du dich bewirbst. Am besten ist es, wenn du schon im Vorfeld einen Deutschkurs belegst. 
+                                            <br/>
+                                            Es gibt den so genannten „Europäischen Referenzrahmen“ – damit kann man das Sprachlevel international vergleichen. Es empfiehlt sich, hier das Niveau B2 zu haben. Informiere dich zum Beispiel beim „Goethe Institut“ 
+                                        </div>
+                                        المطلوب من الناس المهتميه بالتدريب المهنى تعتبر البوست بدايه لفتح الباب
+                                        <br/>
+                                        ______________________________________
+                                        <br/>
+                                        <a href='https://www.facebook.com/groups/311111149267612/posts/444198775958848/'>بوست كمان عن الاوسبيلدونج</a>
                                     </h3>
                                 </div>
                             </div>
