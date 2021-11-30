26fb18c898067c6bbdf7cdfc4a0b0849a9ed0855
ToGermanScore Ltd <yousef@togermanscore.com>
2021-11-30 10:50:36 +0200

    make it better

--- src/components/LebenInDeutschland/ForGirls/ForGirls.js
+++ src/components/LebenInDeutschland/ForGirls/ForGirls.js
@@ -4,7 +4,7 @@ import './ForGirls.css';
 class ForGirls extends React.Component {
     render() {
         return (
-            <article className="article br3 ba b--black-10 w-100 w-50-m w-50-l mw7 shadow-5 center">
+            <article className="article br3 ba b--black-10 w-100 w-50-m w-50-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -16,6 +16,13 @@ class ForGirls extends React.Component {
                             <div className="ui form ma0">
                                 <div className="fields">
                                     <h3>
+                                        تجارب للبنات موجودين حاليا في المانيا دي تجميعه لتلات تجارب من الناس اللي موجوده بالفعل ودي فيديوهات بتحكي عن تجربتهم
+                                        <br/>
+                                        <a target='_blank' rel='noreferrer' href='https://youtu.be/drU0asT2ilU'>أحمد سمير مع ندى بسيونى</a>
+                                        <br/>
+                                        <a target='_blank' rel='noreferrer' href='https://youtu.be/1MEgBgN0N_g'>عبدالله حسن مع نور هاشم</a>
+                                        <br/>
+                                        <a target='_blank' rel='noreferrer' href='https://youtu.be/oTEr9C8Ph0M'>DW حلقة  آلاء أبو جاسر من غزة  مع قناة</a>
                                     </h3>
                                 </div>
                             </div>
