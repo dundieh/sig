2bf42e70d9582eaeb99ef6eda5d04c8638f68d23
ToGermanScore Ltd <yousef@togermanscore.com>
2021-12-17 22:07:54 +0200

    make it better

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,34 +5,49 @@ class Navig extends React.Component {
     
     render() {
         return (
-                    <header role="banner">
-                        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                            <div class="container">
-                            <a class="navbar-brand" href="/">Studiend</a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarsExample05">
-                                <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/about">About</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/contact">Contact</a>
-                                </li>
-                                </ul>
-                                <ul class="navbar-nav ml-auto">
-                                <li class="nav-item cta-btn">
-                                    <a class="nav-link" target='_blank' rel='noreferrer' href="https://www.facebook.com/groups/311111149267612">Facebook Group</a>
-                                </li>
-                                </ul>
-                            </div>
-                            </div>
-                        </nav>
-                    </header>
+            <nav role="navigation">
+                <div id="menuToggle">
+                    <input type="checkbox" />
+
+                    <span></span>
+                    <span></span>
+                    <span></span>
+
+                    <ul id="menu">
+                    <a href="/"><li>Home</li></a>
+                    <a href="/about"><li>About</li></a>
+                    <a href="/contact"><li>Contact</li></a>
+                    </ul>
+                </div>
+            </nav>
+                    // <header role="banner">
+                    //     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                    //         <div class="container">
+                    //         <a class="navbar-brand" href="/">Studiend</a>
+                    //         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                    //             <span class="navbar-toggler-icon"></span>
+                    //         </button>
+                    //         <div class="collapse navbar-collapse" id="navbarsExample05">
+                    //             <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
+                    //             <li class="nav-item">
+                    //                 <a class="nav-link" href="/">Home</a>
+                    //             </li>
+                    //             <li class="nav-item">
+                    //                 <a class="nav-link" href="/about">About</a>
+                    //             </li>
+                    //             <li class="nav-item">
+                    //                 <a class="nav-link" href="/contact">Contact</a>
+                    //             </li>
+                    //             </ul>
+                    //             <ul class="navbar-nav ml-auto">
+                    //             <li class="nav-item cta-btn">
+                    //                 <a class="nav-link" target='_blank' rel='noreferrer' href="https://www.facebook.com/groups/311111149267612">Facebook Group</a>
+                    //             </li>
+                    //             </ul>
+                    //         </div>
+                    //         </div>
+                    //     </nav>
+                    // </header>
         );
     }
 }
