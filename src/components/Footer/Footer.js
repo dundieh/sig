8eda7a4713eae327e57c131c2e0373c185502833
ToGermanScore Ltd <yousef@togermanscore.com>
2022-06-29 12:53:28 +0200

    init

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,21 +4,26 @@ import './Footer.css';
 class Footer extends React.Component {
     render() {
         return (
-            <footer class="bg-dark text-center text-white">
-              <div class="container p-4 pb-0">
-                <section class="mb-4">
-                  <a class="btn btn-outline-light btn-floating m-1" target='_blank' rel="noreferrer" href="https://www.facebook.com/groups/311111149267612" role="button"><i class="fab fa-facebook-f"></i></a>
-
-                  <a class="btn btn-outline-light btn-floating m-1" target='_blank' rel="noreferrer" href="https://www.instagram.com/5mahrous/" role="button"><i class="fab fa-instagram"></i></a>
-                  <a class="btn btn-outline-light btn-floating m-1" target='_blank' rel="noreferrer" href="https://www.twitter.com/5Mahrous/" role="button"><i class="fab fa-twitter"></i></a>
-                  <a class="btn btn-outline-light btn-floating m-1" target='_blank' rel="noreferrer" href="https://github.com/yousefmahrous" role="button"><i class="fab fa-github"></i></a>
-                </section>
-              </div>
-
-              <div class="text-center p-3 cright">
-                Copyright ©2021 <a class="text-white" target='_blank' rel="noreferrer" href='https://mahrous.myportfolio.com/'>Yousef Mahrous</a>. All rights reserved
-              </div>
-            </footer>
+            <footer class="footer">
+              <a href="mailto:contact@gerat.co" target="_blank" class="footer__link" rel="noreferrer">contact@gerat.co</a>
+              <ul class="social-list">
+                  <li class="social-list__item">
+                      <a href="https://instagram.com/dundieh" target="_blank" class="social-list__link" rel="noreferrer">
+                          <i class="fab fa-instagram"></i>
+                      </a>
+                  </li>
+                  <li class="social-list__item">
+                      <a href="https://twitter.com/dundieh" target="_blank" class="social-list__link" rel="noreferrer">
+                          <i class="fab fa-twitter"></i>
+                      </a>
+                  </li>
+                  <li class="social-list__item">
+                      <a href="https://github.com/dundieh" target="_blank" class="social-list__link" rel="noreferrer">
+                          <i class="fab fa-github"></i>
+                      </a>
+                  </li>
+              </ul>
+          </footer>
         );
     }
 }
