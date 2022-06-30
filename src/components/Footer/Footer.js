7076510017fb5930a830d88440ffe50bcff0c420
ToGermanScore Ltd <yousef@togermanscore.com>
2022-06-30 18:15:06 +0200

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,24 +5,16 @@ class Footer extends React.Component {
     render() {
         return (
             <footer class="footer">
-              <a href="mailto:contact@gerat.co" target="_blank" class="footer__link" rel="noreferrer">contact@gerat.co</a>
               <ul class="social-list">
-                  <li class="social-list__item">
-                      <a href="https://instagram.com/dundieh" target="_blank" class="social-list__link" rel="noreferrer">
-                          <i class="fab fa-instagram"></i>
-                      </a>
-                  </li>
-                  <li class="social-list__item">
-                      <a href="https://twitter.com/dundieh" target="_blank" class="social-list__link" rel="noreferrer">
-                          <i class="fab fa-twitter"></i>
-                      </a>
-                  </li>
-                  <li class="social-list__item">
-                      <a href="https://github.com/dundieh" target="_blank" class="social-list__link" rel="noreferrer">
-                          <i class="fab fa-github"></i>
-                      </a>
-                  </li>
+                  <p>&copy;2022 Yousef Mahrous. All rights reserved.<a href="https://github.com/dundieh/studium" target="_blank" class="social-list__link" rel="noreferrer">(source code)</a></p>
               </ul>
+              {/* <ul class="social-list">
+                  <li class="social-list__item">
+                      <a href="https://github.com" target="_blank" class="social-list__link" rel="noreferrer">
+                        <i className='fa fa-gtihub'></i>
+                      </a>
+                  </li>
+              </ul> */}
           </footer>
         );
     }
