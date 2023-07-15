5748a751fe365f95a514f6b6a733afba0c3d72cb
Yousef Mahrous <yousefzentral@gmail.com>
2023-07-15 14:19:41 +0300

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
         return (
             <footer class="footer">
               <ul class="social-list">
-                  <p>&copy; 2022 Yousef Mahrous. All rights reserved.<a href="https://github.com/yousefmhrs/studium" target="_blank" class="social-list__link" rel="noreferrer">(source code)</a></p>
+                  <p>&copy; 2023 Yousef Mahrous. All rights reserved.<a href="https://github.com/yousefnc/studium" target="_blank" class="social-list__link" rel="noreferrer">(source code)</a></p>
               </ul>
             </footer>
         );
